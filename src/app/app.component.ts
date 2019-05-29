4d265ae2dcef844d40910e39aae2b0d76e50b9b1
Aleksandr Radchenko <Aleksandr_Radchenko@epam.com>
2019-05-29 10:22:53 +0300

    pt5 - add route to heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Tour of Heroes';
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.messageService.add('AppComponent: initialized');
+  }
 }
